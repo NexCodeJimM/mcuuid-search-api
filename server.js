5d1b03bb59dc93c65befb2229a4f685cebd2b4a2
Jim Mendoza <jim@nexcode.dev>
2023-02-13 13:31:08 +0400

    edited port.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const corsOptions = require("./config/corsOptions"); // Calling corsOptions
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 3501;
 
 app.use(cors(corsOptions));
 
